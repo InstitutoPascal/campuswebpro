9ef9ee84162270458c921376dc8d7f14ac6f0d7d
Yanina Alarcon <yo.yani.alarcon13@gmailcom>
2013-04-12 20:13:05 -0300

    agrego plantilla de horarios para alumnos

---
+++ controllers/alumnos.py
@@ -0,0 +1,7 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+#esta funcion es de prueba
+def horarios():
+    "Listado de horarios por materia"
+    return {}
---
+++ controllers/docentes.py
@@ -0,0 +1,8 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+
+
+def horarios():
+    ""
+    return {}

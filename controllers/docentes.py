38c8e7ac4921425d0cc2d00917c4f0eeb6c76659
hector <hector.e.calvo@gmail.com>
2013-04-17 19:40:32 -0300

    tabla de  horarios de docentes de 3ro sistemas

---
+++ controllers/docentes.py
@@ -0,0 +1,12 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+def asistencias():
+    return{}
+
+def horarios():
+    ""
+    return {}
+    
+def examenes_finales():
+    return{}

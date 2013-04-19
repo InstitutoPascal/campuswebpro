ae5d2323f39abf5bae81a2733d3bf6bf29b13504
IvanaSantillo <lahoradeyvana@hotmail.com>
2013-04-19 19:31:29 -0300

    notas cuatrimestrales practica profesional de tercero

---
+++ controllers/alumnos.py
@@ -0,0 +1,11 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+#esta funcion es de prueba
+def horarios():
+    "Listado de horarios por materia"
+    return {}
+    
+def inasistencias():
+    "Listado con inasistencias del alumno"
+    return {}

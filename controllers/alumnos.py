13c6402f9c6d2f6df937836952f691bdf13ef23f
lucas gonzalez <luka.jesu12@gmail.com>
2013-04-19 19:51:13 -0300

    agrego formulario de inscripcion a final

---
+++ controllers/alumnos.py
@@ -0,0 +1,19 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+#esta funcion es de prueba
+def horarios():
+    "Listado de horarios por materia"
+    return {}
+    
+def inasistencias():
+    "Listado con inasistencias del alumno"
+    return {}
+    
+def finales():
+    "Listado de Examenes Rendidos"
+    return {}
+    
+def examen_f():
+    "formulario inscripcion a examen final"
+    return {}

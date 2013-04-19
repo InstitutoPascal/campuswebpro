ed88aaebec768cfcbabc71b0f68a14789001eb6f
lucas gonzalez <luka.jesu12@gmail.com>
2013-04-19 19:08:55 -0300

    agrego lista de finales

---
+++ controllers/alumnos.py
@@ -0,0 +1,15 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+#esta funcion es de prueba
+def horarios():
+    "Listado de horarios por materia"
+    return {}
+    
+def inasistencias():
+    "Listado con inasistencias del alumno"
+    return {}
+    
+def finales():
+    "Listado de Examenes Rendidos"
+    return {}
---
+++ controllers/tramites.py
@@ -0,0 +1,2 @@
+def constancia ():
+    return {}

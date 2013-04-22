da5073a99a8792eaf7ce284193b64c089456f809
lucas gonzalez <luka.jesu12@gmail.com>
2013-04-22 13:13:42 -0300

    agrego listado de examenes cuatrimestrales rendidos

---
+++ controllers/alumnos.py
@@ -0,0 +1,23 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+#esta funcion es de prueba
+def horarios():
+    "Listado de horarios por materia"
+    return {}
+    
+def inasistencias():
+    "Listado con inasistencias del alumno"
+    return {}
+    
+def finales():
+    "Listado de Examenes Rendidos"
+    return {}
+    
+def examen_f():
+    "formulario inscripcion a examen final"
+    return {}
+    
+def parcial():
+    "listado de examenes cuatrimestrales rendidos"
+    return{}

d7326755cce8164d4b1b51aade920be190cd9f86
Mauro Schiavone <mauroschiavone2012@gmail.com>
2013-04-24 20:08:32 -0300

    actualizo tramites (iniciar, consultar y actualizar)

---
+++ controllers/docentes.py
@@ -0,0 +1,15 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+def asistencias():
+    return{}
+
+def horarios():
+    ""
+    return {}
+    
+def finales():
+    return{}
+    
+def examenes_cuatrimestrales():
+    return{}
---
+++ controllers/tramites.py
@@ -0,0 +1,15 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+
+def iniciar():
+    return{}
+
+def consultar():
+    "Listado de certificados"
+    return {}
+
+def actualizar():
+    # agrega comentarios o cambia estado del tramite
+    return {}
+

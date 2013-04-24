74d49af06e100dad89ae38f19c3e34d648e661bf
Sofia Ramirez <sofiaramirez229@gmail.com>
2013-04-24 19:06:51 -0300

    modifico nombre de la tabla examenes.html

---
+++ controllers/alumnos.py
@@ -0,0 +1,26 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+#esta funcion es de prueba
+def horarios():
+    "Listado de horarios por materia"
+    return {}
+    
+def inasistencias():
+    "Listado con inasistencias del alumno"
+    return {}
+    
+def finales():
+    "Listado de Examenes Rendidos"
+    return {}
+    
+def final():
+    "formulario inscripcion a examen final"
+    return {}
+    
+def parcial():
+    "listado de examenes cuatrimestrales rendidos"
+    return{}
+def inscripciones():
+    "Listado de inscripciones"
+    return {}

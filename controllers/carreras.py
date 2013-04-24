0a5666ec7b17d51849df6545b5114f44b314c0fb
Sofia Ramirez <sofiaramirez229@gmail.com>
2013-04-24 19:44:01 -0300

    modifico nombre de tabla

---
+++ controllers/carreras.py
@@ -0,0 +1,7 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+
+def sistemas(): return plugin_flatpage()
+def rrhh(): return plugin_flatpage()
+def salud(): return plugin_flatpage()

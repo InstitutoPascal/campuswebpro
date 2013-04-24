13c43e1a7028d7336d2196a6371992ddfe36e5c5
IvanaSantillo <lahoradeyvana@hotmail.com>
2013-04-24 20:02:24 -0300

    agrego menu

---
+++ controllers/tramites.py
@@ -0,0 +1,14 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+def constancia():
+    return{}
+def certificado():
+    "Listado de certificados"
+    return {}
+def titulo():
+    "Listado de Titulos en Tramite"
+    return {}
+def tramites_varios():
+    "Listado de Tramites Varios"
+    return {}

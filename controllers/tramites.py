29665f15f206b2a18de60e6fd9248d07389f91d8
Sofia Ramirez <sofiaramirez229@gmail.com>
2013-04-24 19:24:41 -0300

    merge

--- controllers/tramites.py
+++ controllers/tramites.py
@@ -1,2 +1,14 @@
-def constancia ():
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+def constancia():
+    return{}
+def certificado():
+    "Listado de certificados"
+    return {}
+def titulo():
+    "Listado de Titulos en Tramite"
+    return {}
+def tramites_varios():
+    "Listado de Tramites Varios"
     return {}

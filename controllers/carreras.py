f7d1fe8a400c9d7d62a696175f8fffc9bfe8c5aa
Ezequiel <ezevillalba92@gmail.com>
2013-04-26 19:40:44 -0300

    cntroladr

--- controllers/carreras.py
+++ controllers/carreras.py
@@ -2,6 +2,35 @@
 # try something like
 def index(): return plugin_flatpage()
 
-def sistemas(): return plugin_flatpage()
-def rrhh(): return plugin_flatpage()
-def salud(): return plugin_flatpage()
+def carreras():
+    return{}
+
+def sistemas(): 
+    return {}
+
+def salud():
+    return {}
+    
+def salud_enfermeria():
+    return {}
+
+def salud_cardiologia():
+    return {}
+    
+def salud_laboratorio():
+    return {}
+    
+def salud_auxenfermeria():
+    return {}
+    
+def salud_instrumentacion():
+    return {}
+    
+def salud_laboratorio():
+    return {}
+    
+def salud_radiologia():
+    return {}
+
+def redes(): 
+    return {}

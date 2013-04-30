a90408fe5eeec5b65b7dd6caaa51958a171996e3
IvanaSantillo <lahoradeyvana@hotmail.com>
2013-04-30 19:59:26 -0300

    agrego vista de apuntes

---
+++ controllers/carreras.py
@@ -0,0 +1,36 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+
+def carreras():
+    return{}
+
+def sistemas(): 
+    return {}
+
+def salud():
+    return {}
+    
+def salud_enfermeria():
+    return {}
+
+def salud_cardiologia():
+    return {}
+    
+def salud_laboratorio():
+    return {}
+    
+def salud_auxenfermeria():
+    return {}
+    
+def salud_instrumentacion():
+    return {}
+    
+def salud_laboratorio():
+    return {}
+    
+def salud_radiologia():
+    return {}
+
+def redes(): 
+    return {}
---
+++ controllers/docentes.py
@@ -0,0 +1,24 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+def asistencias():
+    return{}
+
+def horarios():
+    ""
+    return {}
+    
+def finales():
+    return{}
+    
+def parciales():
+    return{}
+    
+    
+def planificaciones():
+    ""
+    return {}
+    
+def apuntes():
+    ""
+    return {}

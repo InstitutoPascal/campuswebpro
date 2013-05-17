15a188a67deb1219f6ae355db40736a47021982f
Mariano Reingart <reingart@gmail.com>
2013-05-17 19:53:02 -0300

    subo el respaldo de la base de datos demo

---
+++ controllers/docentes.py
@@ -0,0 +1,28 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+def asistencias():
+    return{}
+
+def horarios():
+    ""
+    return {}
+    
+def finales():
+    return{}
+    
+def parciales():
+    return{}
+    
+    
+def planificaciones():
+    ""
+    return {}
+    
+def apuntes():
+    ""
+    return {}
+
+def recursos():
+    ""
+    return{}

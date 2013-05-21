b847fda39dd46e254067dda914e8ba57c5f3c568
lucas gonzalez <luka.jesu12@gmail.com>
2013-05-21 17:55:51 -0300

    defino funcion para posterior insercion en tabla alumnos

--- controllers/setup_alumnos.py
+++ controllers/setup_alumnos.py
@@ -1,30 +1,7 @@
 # coding: utf8
 # intente algo como
 def index(): return dict(message="hello from setup_alumnos.py")
-####### FUNCIONES DE PRUEBAS---INSERT EN TABLAS---######
-def horarios():
-    "Listado de horarios por materia"
-    return {}
-    
-def inasistencias():
-    "Listado con inasistencias del alumno"
-    return {}
-    
-def examenes():
-    "Listado de Examenes finales Rendidos"
-    return {}
-    
-def final():
-    "formulario inscripcion a examen finales aptos para el alumno"
-    return {}
-    
-def parciales():
-    "listado de examenes cuatrimestrales rendidos"
-    return{}
-def inscripciones():
-    "Listado de inscripciones"
-    return {}
-    
-def archivos():
-    "descarga de archivos pedagogicos subidos por docentes"
+####### FUNCION DE PRUEBA---INSERT EN TABLA ALUMNOS---######
+def insert_alumnos():
+    "funcion setup"
     return {}

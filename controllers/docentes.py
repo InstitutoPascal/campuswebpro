9d01e1395175de91b6ed120342911527513ce8bc
lucas gonzalez <luka.jesu12@gmail.com>
2013-05-24 19:07:19 -0300

    agrego insert en tabla alumnos

---
+++ controllers/docentes.py
@@ -0,0 +1,20 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+def asistencias():
+    return{}
+
+def horarios():
+    ""
+    return {}
+    
+def finales():
+    return{}
+    
+def parciales():
+    return{}
+def apuntes():
+    return{}
+    
+def recursos():
+    return{}

158cee0f1b742f07cc3ff03fa1a3859cd49524e1
hector <hector.e.calvo@gmail.com>
2013-05-24 19:33:15 -0300

    Controlador setup docentes

---
+++ controllers/docentes.py
@@ -0,0 +1,26 @@
+# coding: utf8
+# try something like
+#como hacer funcar esto
+def index(): return plugin_flatpage()
+def asistencias():
+    ""
+    return{}
+
+def horarios():
+    ""
+    return {}
+    
+def finales():
+    ""
+    return{}
+    
+def parciales():
+    ""
+    return{}
+def apuntes():
+    ""
+    return{}
+    
+def recursos():
+    ""
+    return{}
---
+++ controllers/setup_alumnos.py
@@ -0,0 +1,7 @@
+# coding: utf8
+# intente algo como
+def index(): return dict(message="hello from setup_alumnos.py")
+####### FUNCION DE PRUEBA---INSERT EN TABLA ALUMNOS---######
+def insert_alumnos():
+    "funcion setup"
+    return {}

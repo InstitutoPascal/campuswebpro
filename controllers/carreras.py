0ecde956726f0dca527119b7b42f80836dd34f58
hector <hector.e.calvo@gmail.com>
2013-06-05 19:35:15 -0300

    carreras

---
+++ controllers/carreras.py
@@ -0,0 +1,40 @@
+# coding: utf8
+# try something like
+def index():
+    q=db.carreras.carreraid>0
+    carreras=db(q).select()
+    return{'carreras':carreras}
+
+def carreras():
+
+    return{}
+
+def sistemas():
+    return {}
+
+def salud():
+    return {}
+
+def salud_enfermeria():
+    return {}
+
+def salud_cardiologia():
+    return {}
+
+def salud_laboratorio():
+    return {}
+
+def salud_auxenfermeria():
+    return {}
+
+def salud_instrumentacion():
+    return {}
+
+def salud_laboratorio():
+    return {}
+
+def salud_radiologia():
+    return {}
+
+def redes():
+    return {}

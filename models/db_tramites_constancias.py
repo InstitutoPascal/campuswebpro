4e6343a7092232ea46fcadffbe1b9d77e8aae1e7
Raul Elias Sosa <eliassosa@users.noreply.github.com>
2013-06-06 00:59:39 +0200

    agrego bases de datos de tramites constancias

---
+++ models/db_tramites_constancias.py
@@ -0,0 +1,20 @@
+# coding: utf8
+
+db.define_table('constancia_modelo',
+    Field('modeloid', type='id'),
+    Field('desccripcion', type='string', length=200, default="certificado alumno regular", comment=T('tipo de contancia')),
+    Field('plantilla', type='text', comment=T('texto base')),
+    #format="%(descripcion)s",
+    #primarykey=["modeloid"],
+    )
+
+db.define_table('constancia',
+    Field('id', type='id'),
+    Field("modeloid", db.constancia_modelo),
+    Field('alumnoid', db.alumnos, label=T('Alumno')),
+    Field('materias', type='text', length=200,label=T('porcentaje de materias aprobadas')),
+    Field('texto', type='string', length=500, label=T('tipo de certificado')),
+    Field('fechade_inicio', type='date', length=200,label=T('Fecha De Inicio')),
+    Field('fechade_entrega', type='date', length=50, label=T('Fecha De Entrega')),    
+    #primarykey=["id"],
+    )

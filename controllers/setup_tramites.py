b991c37f515dcf4c3c425585bc980cdd3a8a4cb0
Mauro Schiavone <mauroschiavone2012@gmail.com>
2013-06-12 19:43:24 -0300

    agrego insert de la tabla ubicaciones

--- controllers/setup_tramites.py
+++ controllers/setup_tramites.py
@@ -14,3 +14,21 @@ def cargar_tramites():
     # Devuelvo una consulta del contenido de la tabla
    
     return {'filas': db(db.tramites.tramiteid>0).select()}
+
+
+def cargar_ubicaciones():
+    db(db.ubicaciones.ubicacionid>0).delete()  
+
+    # Insertamos las ubicaciones requeridas
+    
+    db.ubicaciones.insert(ubicacionid="1",descripcion="Certificado de Alumno Regular", telefono="456783",email="jose@hotmail.com")
+    db.ubicaciones.insert(ubicacionid="2",descripcion="Certificado de Titulo en Tramite", telefono="439827",email="juan@hotmail.com")
+    db.ubicaciones.insert(ubicacionid="3",descripcion="Certificado de Reincorporacion", telefono="459382",email="mario@hotmail.com")
+    db.ubicaciones.insert(ubicacionid="4",descripcion="Certificado de Materias Aprobadas", telefono="438297",email="martin@hotmail.com")
+    
+    
+    response.view="generic.html"
+    
+    # Devuelvo una consulta del contenido de la tabla
+   
+    return {'filas': db(db.ubicaciones.ubicacionid>0).select()}

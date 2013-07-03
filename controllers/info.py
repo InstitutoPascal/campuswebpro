4391f31dbedf43a85ab1e4855159641ac85f903e
yanina alarcon <yo.yani.alarcon13@gmail.com>
2013-07-03 20:15:24 -0300

    modificacion de inasistencias

---
+++ controllers/info.py
@@ -0,0 +1,8 @@
+# coding: utf8
+# try something like
+def index(): return plugin_flatpage()
+
+def historia(): return plugin_flatpage()
+def por_que_elegirnos(): return plugin_flatpage()
+def plantel_docente(): return plugin_flatpage()
+def director(): return plugin_flatpage()

65d46d03d8313ee5288d482e931422365d738b02
sofia ramirez <sofiaramirez229@gmail.com>
2013-07-29 19:45:26 -0300

    modifico vista de archivos

---
+++ controllers/directivos.py
@@ -0,0 +1,13 @@
+# coding: utf8
+# intente algo como
+def index(): return dict(message="hello from directivo.py")
+
+
+def constancia_de_alumno_regular():
+    return {}
+    
+def certificados_de_materias_aprobadas():
+    return {}
+    
+def reincorporacion_de_alumnos():
+    return {}

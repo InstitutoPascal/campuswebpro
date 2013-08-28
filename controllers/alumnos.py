14d3ad915375d577003ef9f252e472d1d720fd39
Ivana <lahoradeyavana@hotmail.com>
2013-08-28 19:04:30 -0300

    asistencias

---
+++ controllers/alumnos.py
@@ -0,0 +1,167 @@
+# coding: utf8
+# try something like
+
+
+
+
+#@auth.requires_login()
+
+def index(): 
+    "menu alumnos"
+    return dict ()
+
+@auth.requires_login()
+@auth.requires_membership(role='alumnos')
+def ficha():
+           
+    # obtengo el registro del alumno ya registrado como usuario 
+  
+    q = db.alumnos.user_id== auth.user_id
+ 
+    alumno= db(q).select(db.alumnos.nombre, db.alumnos.sexo, 
+                         db.alumnos.foto, db.alumnos.email1,
+                         db.alumnos.localidad)
+                         
+    return dict (alumno=alumno, q=q)
+    
+#@auth.requires_login()
+
+
+@auth.requires_login()
+def ingreso():
+    db.alumnos.user_id.default= auth.user_id
+    subtitulo= T ('Complete el formulario por favor...')
+    form=SQLFORM(db.alumnos)
+    #db.auth_membership.insert(auth_membership.user_id== db.auth_id, auth_membership.group_id== auth_group.id)
+    if form.accepts(request.vars,session):
+        db.auth_membership.insert( auth_membership.user_id== auth.user_id, auth_membership.group_id== 'alumnos')
+        response.flash='Usted fue agregado como alumno...'
+    elif form.errors: 
+        response.flash='Hay errores en el formulario!'
+    else:
+        response.flash='Por favor, complete el formulario!'
+        
+    return dict (form=form, sub=subtitulo)
+    
+    
+    
+
+def busqueda():
+    # armo un formulario para buscar alumno por su dni
+    form = SQLFORM.factory(
+        Field("dni", "integer"),
+        Field("nombre", "string"),
+        )
+    q= db.alumnos.id>0
+    if form.accepts(request.vars, session):
+        # buscar el alumno
+        q = db.alumnos.dni == form.vars.dni
+        q&= db.alumnos.nombre == form.vars.nombre
+        alumno = db(q).select().first()
+        if alumno:
+            # encontrado, redirigo al menu alumnos en index 
+            redirect(URL(f=index, vars={'alumnoid': alumno.alumnoid}))
+            redirect(URL(f=examenes, vars={'alumnoid': alumno.alumnoid}))
+            redirect(URL(f=parciales, vars={'alumnoid': alumno.alumnoid}))
+            
+        else:
+            response.flash = "Alumno no encontrado"
+    #response.view = "generic.html"  # HACER una vista de verdad
+    return dict (form = form)
+
+def horarios():
+    q= db.horarios.horaid==db.horas.horaid
+    q &= db.horarios.comisionid== db.comisiones.comisionid
+    q &= db.comisiones.personalid== db.personal.personalid
+    q &= db.comisiones.materiaid== db.materias.materiaid
+    q &= db.comisiones.divisionid== db.divisiones.divisionid
+    filas= db(q).select(db.horas.hora, db.personal.nombre, db.materias.nombre, db.divisiones.divisionid, db.horarios.dia)
+    
+    
+    horario = {'lunes':{},'martes':{},'miercoles':{},'jueves':{},'viernes':{}}
+    # horario es una estructura cuya clave es el dia y el valor es otro diccionario....
+    #  {'lunes': {1: fila} ... }
+    for fila in filas:
+        horario[fila.horarios.dia].setdefault(fila.horas.hora, {})[fila.divisiones.divisionid]= fila
+        
+    return dict (horario=horario)
+    
+   
+        
+    
+def inasistencias():
+    alumno= db.alumnos.user_id== auth.user_id
+    q= db.faltas.faltaid== db.faltas.faltaid
+    q &= db.comisiones.materiaid== db.materias.materiaid
+    q &= db.comisiones.personalid== db.personal.personalid
+    q &= db.faltas.alumnoid== db.alumnos.alumnoid
+
+    
+    falta= db(q).select(db.alumnos.nombre, db.materias.nombre, db.personal.nombre, db.faltas.cantidad, db.faltas.fecha)
+   
+    
+    return dict (falta=falta)
+    
+def examenes():
+    # obtengo el parámetro pasado por variable en la url
+    alumnoid = request.vars['alumnoid']
+    # busco el alumno
+    alumno = db.alumnos[alumnoid]
+    # obtengo la carrera / plan de estudio
+    #carreraid = alumno.carreraid ...
+    
+    # busco las materias:
+    q = db.materias.materiaid>0  # HACER: filtrar por carrera/plan de estudio
+    orden = db.materias.materiaid    # HACER: mejorar el orden (por ej. curso)
+    materias = db(q).select(orderby=orden)
+    
+    # obtengo las notas
+    q = db.notas.alumnoid == db.alumnos.alumnoid
+    q &= db.notas.materiaid == db.materias.materiaid
+    q &= db.notas.calificacionid == 5  # filtrar solo finales
+    #q &= db.notas.periodoid == ...     # HACER: filtrar otros campos
+    notas = db(q).select(db.alumnos.nombre, db.materias.nombre, db.notas.nota, db.notas.alta)
+    
+    return dict (notas= notas)
+    
+def final():
+     # a = db().select(db.examenes.ALL)#lista todos
+   q= db.examenes.examenid>0
+   q &= db.examenes.materiaid== db.materias.materiaid
+   q &= db.examenes.personalid1== db.personal.personalid
+    
+
+    
+   final= db(q).select(db.materias.nombre, db.personal.nombre, db.examenes.fecha, db.examenes.hora)
+  
+   return dict (final=final) 
+  
+            
+         
+###################################################################################
+        
+def parciales():
+    # obtengo el parámetro pasado por variable en la url
+    alumnoid = request.vars['alumnoid']
+    # busco el alumno
+    alumno = db.alumnos[alumnoid]
+    # obtengo la carrera / plan de estudio
+    #carreraid = alumno.carreraid ...
+    
+    # obtengo las notas
+    q = db.notas.alumnoid == db.alumnos.alumnoid
+    q &= db.notas.materiaid == db.materias.materiaid
+    q &= db.notas.calificacionid == 3  # filtrar solo cuatrimestrales
+    q &= db.notas.periodoid == db.periodos.periodoid     # HACER: filtrar otros campos
+    notas = db(q).select(db.alumnos.nombre, db.materias.nombre, 
+    db.notas.nota, db.periodos.descripcion, db.notas.fecha)
+    
+    return dict (notas= notas)
+    
+def inscripciones():
+    "Listado de inscripciones a curso"
+    return {}
+    
+def archivos():
+    "descarga de archivos pedagogicos subidos por docentes"
+    return {}
---
+++ controllers/docentes.py
@@ -0,0 +1,153 @@
+# coding: utf8
+# try something like
+#como hacer funcar esto
+def busqueda():
+    # armo un formulario para buscar alumno por su dni
+    form = SQLFORM.factory(
+        Field("dni", "integer"),
+        Field("nombre", "string"),
+        )
+    q= db.profesores.id>0
+    if form.accepts(request.vars, session):
+        # buscar el alumno
+        if form.vars.dni:
+            q &= db.personal.dni == form.vars.dni
+        if form.vars.nombre:    
+            q &= db.personal.nombre.contains(form.vars.nombre)
+        docente = db(q).select().first()
+        
+        
+        if docente:
+            # encontrado, redirigo a cargar notas por 
+            redirect(URL(f=index, vars={'personalid': docente.personal.personalid}))
+            
+        else:
+            response.flash = "docente no encontrado"
+    #response.view = "generic.html"  # HACER una vista de verdad
+    return dict (form = form)
+    
+    
+def index():
+    if request.vars:
+        # si me pasan en la URL el docente, lo filtro 
+        q=db.personal.personalid == request.vars['personalid']
+
+        redirect(URL(f=ficha, vars={'personalid': docente.personal.personalid}))
+
+        
+    else:
+        # sino, busco todos los docentes
+        q=db.personal.personalid>0
+    docentes=db(q).select(orderby=db.personal.nombre)
+    return{'docentes':docentes}
+    
+def alumnoXcomision():
+    
+    form=SQLFORM.factory (
+               Field('fecha', 'date', default=request.now.date()),
+               )
+               
+        #cuando hago click en el boton guardar
+    if request.vars.grabar=="GUARDAR":
+            #en k tenemos el nombre del checkbox
+        for _name,_value in request.vars.items():
+            if _name.startswith ("falta"):
+                alumno_id = int(_name[_name.index('_')+1:])
+                comision_id = int(_name[_name.index('_')+1:])
+                inasistencia_id = int(_name[_name.index('_')+1:])
+                fecha = request.now.date()
+
+
+
+                if _value == "on":
+                    db.faltas.insert ( alumnoid= alumno_id, comisionid= comision_id,inasistenciaid=inasistencia_id,cantidad=1)
+
+            
+            
+        
+    if request.vars:
+        # si me pasan en la URL el docente, lo filtro 
+        q=db.alumnos.alumnoid == request.vars['alumnoid']
+        
+    else:
+        # sino, busco todos los docentes
+        q=db.alumnos.alumnoid>0
+    alumnos=db(q).select(orderby=db.alumnos.nombre)
+    #db.faltas.insert(alumnoid= 5, comisionid=1,inasistenciaid="martes",fecha=10)
+
+    return{'alumnos':alumnos, 'form': form}
+    
+
+ 
+
+def horarios():
+    q=db.horarios.horarioid>0
+    horarios=db(q).select()
+    return{'horarios':horarios}
+    
+    
+def finales():
+    q=db.notas.notaid>0
+    notas=db(q).select()
+    return{'notas':notas}
+    
+    
+def parciales():
+    ""
+    return{}
+def apuntes():
+    
+    ""
+    return{}
+    
+def recursos():
+    ""
+    return{}
+    
+def asistencias():
+    
+        form = SQLFORM.factory(
+        Field("materia","string"),)
+        q = db.faltas.id>0
+        q &= db.faltas.comisionid == db.comisiones.comisionid
+        
+        if form.accepts(request.vars, session):
+        
+        
+        
+              q = db.comisiones.comisionid==form.vars.nombre
+              q &= db.faltas.alumnoid==db.alumnos.alumnoid    
+              asistencias=db().select(db.comisiones.nombre, db.alumnos.nombre)
+        else :
+              response.flash="materia no encontrada"
+        
+        return{'asistencias':asistencias}
+
+        
+def ficha():
+    # obtengo el id de la url (primer argumento por posicion):
+   
+    personalid = request.args[0]
+    
+        
+    # obtengo el registro del docente
+    docente = db.personal[personalid]
+    
+    q = db.comisiones.personalid == personalid
+    comisiones = db(q).select()
+  
+    return {'docente':docente, 'comisiones':comisiones}
+    
+    
+    
+def ingreso():
+    db.personal.user_id.default= auth.user_id
+    subtitulo= T ('Complete el formulario por favor...')
+    form=SQLFORM(db.personal)
+    if form.accepts(request.vars,session):
+        response.flash='Usted fue agregado como docente...'
+    elif form.errors: 
+        response.flash='Hay errores en el formulario!'
+    else:
+        response.flash='Por favor, complete el formulario!'
+    return dict (form=form, sub=subtitulo)

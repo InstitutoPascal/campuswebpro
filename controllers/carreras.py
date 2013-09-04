b7037bbef63402bb8c28740a6862b0f0954918b7
Mauro Schiavone <mauroschiavone2012@gmail.com>
2013-09-04 19:11:07 -0300

    arreglo del menu.py

---
+++ controllers/carreras.py
@@ -0,0 +1,47 @@
+# coding: utf8
+# try something like
+def index():
+    q=db.carreras.carreraid>0
+    carreras=db(q).select()
+    return{'carreras':carreras}
+
+def materias():
+    carrerasid=request.args[0]
+    q= db.asignaturas.carreraid==carreraid
+    q &= db.asignaturas.carreraid==db.materias.materiaid
+    q &=db.asiganturas.cursoid==db.cursos.cursoid
+
+    filas=db(q).select()
+    return{'filas':filas}
+
+    return{}
+
+def sistemas():
+    return {}
+
+def salud():
+    return {}
+
+def salud_enfermeria():
+    return {}
+
+def salud_cardiologia():
+    return {}
+
+def salud_laboratorio():
+    return {}
+
+def salud_auxenfermeria():
+    return {}
+
+def salud_instrumentacion():
+    return {}
+
+def salud_laboratorio():
+    return {}
+
+def salud_radiologia():
+    return {}
+
+def redes():
+    return {}

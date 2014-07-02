ac5f6ff3dda7b2e809548ea6cd05351945b300e5
Diego Moreyra <diegojmoreyra@gmail.com>
2014-07-02 20:20:07 -0300

    modifico listarparciales(docentes), creo la tabla que se desea mostrar

---
+++ controllers/cartelera.py
@@ -0,0 +1,13 @@
+# coding: utf8
+# intente algo como
+def index(): return dict(message="hello from cartelera.py")
+def horarios():
+    return {}
+
+
+def examenes():
+    return {}
+    
+    
+def novedades():
+    return {}

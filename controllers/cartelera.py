c0b5d0228b39b0cb93bbdb4782d28c5e8d7d26fe
Agustin Irusta <irusta35@users.noreply.github.com>
2014-07-03 20:28:10 -0300

    cartelera examenes modificado

--- controllers/cartelera.py
+++ controllers/cartelera.py
@@ -2,12 +2,19 @@
 # intente algo como
 def index(): return dict(message="hello from cartelera.py")
 def horarios():
+    response.title = "cartelera"
+    response.subtitle = "horarios"
+    
     return {}
 
 
 def examenes():
+    response.title = "cartelera"
+    response.subtitle = "examenes"
     return {}
     
     
 def novedades():
+    response.title = "cartelera"
+    response.subtitle = "novedades"
     return {}

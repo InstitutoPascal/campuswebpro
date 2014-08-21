69c6a08fd2fec25d2004b6b11b1ace135f11b128
Franco More <franco.more1758@gmail.com>
2014-08-21 20:41:15 -0300

    mejoras en inscripcion a examenes

--- controllers/alumnos.py
+++ controllers/alumnos.py
@@ -327,12 +327,12 @@ def examenes():
 #requiere que haya un usuario logueado e integre el grupo alumnos
 #@auth.requires_membership(role='Alumnos')
 def final():
-    #formulario de inscrip. a comisiones
+    #formulario de inscrip a examenes finales
     q = db.alumnos.user_id== auth.user_id
     #busca y trae todos los datos del alumno logueado
     alumno= db(q).select().first()
     # guarda en una variable los datos para poder ser utilizados y tmb la envio a la vista
-     #cuando hago click en el boton guardar
+    #cuando hago click en el boton guardar
     if request.vars.guardar=="Guardar":
             #en k tenemos el nombre del checkbox
         fecha = request.now.date()
@@ -340,54 +340,82 @@ def final():
         condicion_id= 1
         for _name,_value in request.vars.items():
             if _name.startswith ("final_"):
-                comision_id = int(_name[_name.index('_')+1:])
-                # si el valor es on  en el checkbox insertamos los datos en inscripcion a examenes.
-                if _value== "on":
+                examen_id = int(_name[_name.index('_')+1:])
+                # si el valor es on  en el checkbox insertamos los datos en inscripcion a examenes. 
+                if _value == "on":
                     db.inscripcionesexamen.insert(alumnoid= alumno.alumnoid,
-                                                  alta=fecha,
-                                                  condicion=condicion_id)
+                        examenid= examen_id,
+                        condicion=condicion_id,
+                        alta=fecha,
+                        confirmar=True,
+                        valido=True)
                     ok += 1 #creo contador de examenes insertados/seleccionados por el alumno
         if ok:
-              response.flash= "Usted se a inscripto a %d examenes!" % ok
+              response.flash= "Usted se a inscripto a %d exámenes!" % ok
         else:
               response.flash = "Por favor seleccione una opción!"
     else:
         response.flash='Por favor, complete el formulario'
     # guarda en una variable los datos para poder ser utilizados y tmb la envio a la vista
-    inscripciones = db(db.inscripcionescomision.alumnoid==alumno.alumnoid).select(db.inscripcionescomision.comisionid)
-    inscripciones = [inscripcion.comisionid for inscripcion in inscripciones]
+    inscripciones = db(db.inscripcionesexamen.alumnoid==alumno.alumnoid).select(db.inscripcionesexamen.examenid)
+    inscripciones = [inscripcion.examenid for inscripcion in inscripciones]
     #listo si ya se inscribio al examen
-    aprobada= []
-    desaprobada= []
+    #listo si ya aprobo el examen
+    aprobadas= []
+    desaprobadas= []
     n= db.notas.alumnoid== alumno.alumnoid
     n&= db.notas.calificacionid==5
     nota= db(n).select(db.notas.nota, db.notas.materiaid)
     for notas in nota:
             if notas.nota>4:
-                aprobada.append(notas.materiaid)
+                aprobadas.append(notas.materiaid)
             else:
-                desaprobada.append(notas.materiaid)
-    q = db.comisiones.materiaid== db.materias.materiaid
-    q &= db.comisiones.personalid== db.personal.personalid
+                desaprobadas.append(notas.materiaid)
+    cursadas=[]
+    n= db.inscripcionescomision.alumnoid== alumno.alumnoid
+    n&= db.inscripcionescomision.baja == None
+    n&= db.inscripcionescomision.comisionid== db.comisiones.comisionid
+    inscripciones= db(n).select(db.comisiones.materiaid,
+                                db.inscripcionescomision.condicion,
+                                db.inscripcionescomision.alta, )
+    for inscripcion in inscripciones:
+        cursadas.append(inscripcion.comisiones.materiaid)
+    correlatividades = {}
+    c= db().select(db.correlativas.materiacorrelativa, db.correlativas.materiaid)
+    for x in c:
+        correlatividades.setdefault(x.materiaid, []).append(x.materiacorrelativa)
+
+    q = db.examenes.materiaid== db.materias.materiaid
+    q &= db.examenes.personalid1== db.personal.personalid
     q &= db.materias.materiaid== db.asignaturas.materiaid
     q &= db.asignaturas.carreraid== db.carreras.carreraid
     q &= db.inscripcionescarrera.carreraid== db.carreras.carreraid
     q &= db.inscripcionescarrera.alumnoid== alumno.alumnoid
-    condiciones=db(db.condiciones).select(db.condiciones.condicionid,
-                                          db.condiciones.detalle)
-    comision= db(q).select(db.comisiones.comisionid,
-                           db.comisiones.materiaid,
-                           db.materias.materiaid,
-                           db.materias.nombre,
-                           db.personal.nombre)
 
-    return dict (comision= comision,
-                 alumno=alumno,
-                 inscripciones=inscripciones,
-                 aprobada= aprobada,
-                 desaprobada= desaprobada,
-                 condiciones=condiciones)
+    final= db(q).select(db.examenes.examenid, db.examenes.materiaid, db.materias.nombre, db.personal.nombre, db.examenes.fecha, db.examenes.hora)
 
+    msj_aprobada = {}
+    mensajes = {}
+    msj_inscripto = {}
+    msj_disponible = {}
+    for f in final:
+        msg_aprobada=[]
+        msg = []
+        msg_inscripto= []
+        msg_disponible= []
+
+        if f.examenes.materiaid in correlatividades:
+            correlativas = correlatividades[f.examenes.materiaid]
+            for correlativa in correlativas:
+                if correlativa not in aprobadas:
+                    msg.append("Materia %s No Aprobada" % correlativa)
+            mensajes[f.examenes.materiaid] = ', '.join(msg)
+
+        if f.examenes.materiaid in aprobadas:
+            msg_aprobada.append("Materia Aprobada")
+        msj_aprobada[f.examenes.materiaid] = ', '.join(msg_aprobada)
+
+    return dict ( cursadas=cursadas, final= final, alumno=alumno, inscripciones=inscripciones, aprobadas= aprobadas, desaprobadas= desaprobadas, correlatividades=correlatividades, mensajes=mensajes, msj_aprobada= msj_aprobada, msj_inscripto=msj_inscripto )
 def constancia_final():
     fecha= request.now.date()
     #guardo la fecha actual

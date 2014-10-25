74df33765fbcbb34513ff928d77cbfcad7288a64
Agustin Irusta <irusta35@users.noreply.github.com>
2014-10-25 03:54:54 -0300

    modifico cartelera, consulto base de datos para listar examenes

--- controllers/cartelera.py
+++ controllers/cartelera.py
@@ -22,4 +22,27 @@ def novedades():
 def examenes():
     response.title = "cartelera"
     response.subtitle="Examenes"   
-    return {}
+    CARRERAID=1 #analisis de sistemas
+    CICLOID=5 #ciclo lectivo 2014
+    LLAMADO=1 #PRIMER LLAMADO
+    EXAMEN_PERIODO=30 #EXAMEN DICIEMBRE 2014
+    COMISION_PERIODO=33 #periodo2014
+    
+    q=db.asignaturas.carreraid==CARRERAID
+    q&=db.asignaturas.cicloid==CICLOID
+    q&=db.materias.materiaid==db.asignaturas.materiaid #OBTENEMOS COD Y NOMBRE DE MATERIA
+    q&=db.cursos.cursoid==db.materias.cursoid #OBTENEMOS EL NOMBRE DEL CURSO
+    #OBTENEMOS EL EXAMEN DE ESA MATERIA EN EL PERIODO DICIEMBRE 2014 Y EL PRIMER LLAMADO
+    q&=db.materias.materiaid==db.examenes.materiaid
+    q&=db.examenes.periodoid==EXAMEN_PERIODO
+    q&=db.examenes.llamado==LLAMADO 
+    #OBTENGO LA MATERIA CORRELATIVA DE LA ACTUAL
+    q&=db.materias.materiaid==db.correlativas.materiaid
+    #CONSULTAMOS COMISION ACTUAL DE LA MATERIA EN ESTE PERIODO 2014 PARA VER EL DOCENTE
+    q&=db.materias.materiaid==db.comisiones.materiaid
+    q&=db.comisiones.periodoid==COMISION_PERIODO
+    q&=db.comisiones.personalid==db.personal.personalid
+    filas=db(q).select(db.materias.codigo,db.materias.nombre,db.cursos.nombre, db.examenes.fecha,db.examenes.hora,db.correlativas.materiacorrelativa,db.personal.nombre)
+    
+    
+    return {'filas':filas}

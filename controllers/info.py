46020a99ce15fb3400f4d5d3bd0e40af8b38522f
Raul Elias Sosa <sraulelias@gmail.com>
2015-06-18 19:58:59 -0300

    subo alpicacion en plantel docente

--- controllers/info.py
+++ controllers/info.py
@@ -4,5 +4,9 @@ def index(): return plugin_flatpage()
 
 def historia(): return plugin_flatpage()
 def por_que_elegirnos(): return plugin_flatpage()
-def plantel_docente(): return plugin_flatpage()
+def plantel_docente():
+    filas=db(db.personal.personalid>0).select(db.personal.dni,db.personal.nombre,db.personal.dni)
+  
+    return {'filas':filas}
+
 def director(): return plugin_flatpage()

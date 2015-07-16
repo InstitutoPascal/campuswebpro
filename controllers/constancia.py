f1994a17cfbb5239bcdd7d9d953920562318aba5
MATIAS LEONEL PORTALES <matiiportales@gmail.com>
2015-07-16 05:27:02 -0300

    agrego cambios en formularios de modulo tramites

--- controllers/constancia.py
+++ controllers/constancia.py
@@ -1,6 +1,10 @@
 # coding: utf8
+
 # intente algo como
+response.title = "Campus Web Pro"
+
 def alumno_regular():
+    response.subtitle= "Constancia De Alumno Regular"
     q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
     return {'alumno': alumno}
@@ -8,31 +12,22 @@ def alumno_regular():
 
 
 def materias_aprobadas(): 
+    response.subtitle= "Certificado De Materias Aprobadas"
     q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
     return {'alumno': alumno}
     return {}
 
 def titulo_en_tramite(): 
+    response.subtitle= "Certificado De Titulo En Tramite"
     q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
     return {'alumno': alumno}
     return {}
 
 def reincorporacion(): 
+    response.subtitle= "Reincorporación De Alumno"
     q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
     return {'alumno': alumno}
-  
-
-def alumno_regular2(): 
-    return {}
-
-def materias_aprobadas2(): 
-    return {}
-
-def titulo_en_tramite2(): 
-    return {}
-
-def reincorporacion2(): 
     return {}
--- controllers/tramites.py
+++ controllers/tramites.py
@@ -1,9 +1,14 @@
 # coding: utf8
 # try something like
+response.title= "Campus Web Pro"
+
+
 def index(): return plugin_flatpage()
+
 
 @auth.requires_membership(role='Alumnos')
 def iniciar():
+    response.subtitle= "Menú Iniciar Tramite"
     q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
     #q &=db.alumnos.alumnoid==db.inscripcionescarrera.alumnoid
@@ -13,6 +18,7 @@ def iniciar():
 
 @auth.requires_membership(role='Alumnos')
 def actualizar():
+    response.subtitle= "Menú Actualizar Tramite"
     s = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(s).select().first() #traemos el alumno para notificarlo en la vista
     return {'alumno': alumno}

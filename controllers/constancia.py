fc38938690538f9214057a0447ef2fbef472bf52
NAHUEL COSENTINO <nahue.cosentino5@gmail.com>
2015-07-15 22:47:27 -0300

    agrego actualizaciones en plantillas del modulo tramite

--- controllers/constancia.py
+++ controllers/constancia.py
@@ -1,16 +1,29 @@
 # coding: utf8
 # intente algo como
-def alumno_regular(): 
+def alumno_regular():
+    q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
+    alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
+    return {'alumno': alumno}
     return {}
 
+
 def materias_aprobadas(): 
+    q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
+    alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
+    return {'alumno': alumno}
     return {}
 
 def titulo_en_tramite(): 
+    q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
+    alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
+    return {'alumno': alumno}
     return {}
 
 def reincorporacion(): 
-    return {}
+    q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
+    alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
+    return {'alumno': alumno}
+  
 
 def alumno_regular2(): 
     return {}

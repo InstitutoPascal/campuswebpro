c21cdb4abf4ef416ff881203db1d78c2008e3fc1
MATIAS LEONEL PORTALES <matiiportales@gmail.com>
2015-10-29 23:01:37 -0300

    subo vista nueva con plantilla del modulo tramites

--- controllers/tramites.py
+++ controllers/tramites.py
@@ -79,9 +79,18 @@ def titulo_en_tramite():
     response.subtitle= "Certificado De Titulo En Tramite"
     q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(q).select().first() #traemos el alumno para notificarlo en la vista
-    return {'alumno': alumno}
-    return {}
+    q &=db.alumnos.alumnoid==db.inscripcionescarrera.alumnoid
+    q &=db.inscripcionescarrera.carreraid==db.carreras.carreraid
+    nombre=db(q).select(db.carreras.nombre,
+                         db.carreras.carreraid,
+                         db.inscripcionescarrera.carreraid)
 
+    return dict (
+                 alumno=alumno,
+                 nombre=nombre
+                 )
+    
+    
 @auth.requires_membership(role='Alumnos')
 def reincorporacion(): 
     response.subtitle= "Reincorporación De Alumno"

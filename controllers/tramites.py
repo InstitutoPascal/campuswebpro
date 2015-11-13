72723bb8a90469f9f6f4cbf3919b2c3f65ddf545
MATIAS LEONEL PORTALES <matiiportales@gmail.com>
2015-11-12 21:20:07 -0300

    Modifico menu actualizar, modulo tramite

--- controllers/tramites.py
+++ controllers/tramites.py
@@ -26,20 +26,44 @@ def iniciar():
 
 
 @auth.requires_membership(role='Alumnos')
+#def actualizar():
+    #response.subtitle= "Menú Actualizar Tramite"
+    #s = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
+    #alumno= db(s).select().first() #traemos el alumno para notificarlo en la vista
+    #s &=db.alumnos.alumnoid==db.inscripcionescarrera.alumnoid
+    #s &=db.inscripcionescarrera.carreraid==db.carreras.carreraid
+    #nombre=db(s).select(db.carreras.nombre,
+      #                  db.carreras.carreraid,
+     #                   db.inscripcionescarrera.carreraid)
+
+    #return dict (nombre=nombre, alumno=alumno)
+    
+
+@auth.requires_membership(role='Alumnos')
 def actualizar():
     response.subtitle= "Menú Actualizar Tramite"
+    #q = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
+    #alumno= db(s).select().first() #traemos el alumno para notificarlo en la vista
     s = db.alumnos.user_id== auth.user_id    #guardo en la consulta el registro del alumno
     alumno= db(s).select().first() #traemos el alumno para notificarlo en la vista
     s &=db.alumnos.alumnoid==db.inscripcionescarrera.alumnoid
     s &=db.inscripcionescarrera.carreraid==db.carreras.carreraid
     nombre=db(s).select(db.carreras.nombre,
-                         db.carreras.carreraid,
-                         db.inscripcionescarrera.carreraid)
+                        db.carreras.carreraid,
+                        db.inscripcionescarrera.carreraid)
 
-    return dict (
-                 alumno=alumno,
-                 nombre=nombre
-                 )
+    
+    q = db.tramites.tramiteid==db.expedientes.tramiteid
+    q&= db.expedientes.estadoid==db.estados.estadoid
+    lista=[]
+    lista=db(q).select(db.tramites.tramiteid,
+                       db.tramites.descripcion,
+                       db.estados.descripcion)
+    
+    return dict (lista=lista, nombre=nombre, alumno=alumno)
+    
+    
+
 
 
 @auth.requires_membership(role='Alumnos')
--- models/app_settings.py
+++ models/app_settings.py
@@ -1,5 +1,4 @@
 # coding: utf8 
-DB_URI = "postgres://web2py:1234@localhost/practica7"
-
+DB_URI = "postgres://web2py:1234@localhost/practica6"
 MIGRATE = True            # True: Actualizar tablas según modelos, en produccion poner en Falso para mejor rendimiento
 FAKE_MIGRATE = True       # False: no modificar la base de datos (backup), luego de correr poner en Falso
